07cd699304d365dc4994ab833c3352ed2ae4c0ce
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-30 11:25:23 -0400

    Added Production base url

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,15 @@ const urls = new Map();
 const webSocketUrls = new Map();
 
 urls.set("localhost", "http://localhost:5000");
+urls.set(
+  "poll-react.herokuapp.com",
+  "https://poll-fastify-server.herokuapp.com"
+);
 webSocketUrls.set("localhost", "ws://localhost:5000");
+webSocketUrls.set(
+  "poll-react.herokuapp.com",
+  "wss://poll-fastify-server.herokuapp.com"
+);
 
 export const baseUrl = urls.get(window.location.hostname);
 export const webSocketUrl = webSocketUrls.get(window.location.hostname);
